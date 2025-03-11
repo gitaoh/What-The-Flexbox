f21848c72cc98fbc26cdb2979b46ef080a8c19e7
Zx Tvb <199204954+gitaoh@users.noreply.github.com>
2025-03-11 20:13:47 +0300

    Format code for consistency and improve readability across multiple files

--- autoprefixer/gulpfile.js
+++ autoprefixer/gulpfile.js
@@ -1,14 +1,10 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp");
+var autoprefixer = require("gulp-autoprefixer");
 
-
-gulp.task('styles',function() {
-  gulp.src('css/styles.css')
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
+gulp.task("styles", function () {
+	gulp.src("css/styles.css").pipe(autoprefixer()).pipe(gulp.dest("build"));
 });
 
-
-gulp.task('watch',function() {
-  gulp.watch('css/styles.css', ['styles']);
+gulp.task("watch", function () {
+	gulp.watch("css/styles.css", ["styles"]);
 });
